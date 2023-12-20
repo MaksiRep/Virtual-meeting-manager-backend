3291fd8e4025b5b500b0333b7c89703220f8c3b1
polin <p.osipova1@g.nsu.ru>
2023-12-20 16:58:30 +0700

    Fixed

--- RU.NSU.FIT.VirtualMeetingManager/RU.NSU.FIT.VirtualMeetingManager.Application/Commands/Users/UpdateUserRoles/UpdateUserRolesCommand.cs
+++ RU.NSU.FIT.VirtualMeetingManager/RU.NSU.FIT.VirtualMeetingManager.Application/Commands/Users/UpdateUserRoles/UpdateUserRolesCommand.cs
@@ -13,15 +13,14 @@ public record UpdateUserRolesCommand : IRequest
     public Guid UserId { get; init; }
 
     public IList<Guid> RolesIds { get; init; }
-    
+
     [UsedImplicitly]
     public class UpdateUserRolesCommandHandler : IRequestHandler<UpdateUserRolesCommand>
     {
         private readonly IVMMDbContext _dbContext;
 
         private readonly ICurrentUser _currentUser;
-        
-        
+
         public UpdateUserRolesCommandHandler(IVMMDbContext dbContext, ICurrentUser currentUser)
         {
             _dbContext = dbContext;
@@ -30,13 +29,24 @@ public record UpdateUserRolesCommand : IRequest
 
         public async Task Handle(UpdateUserRolesCommand request, CancellationToken cancellationToken)
         {
-            foreach (var ri in request.RolesIds)
+            if (_currentUser.CanEditUsersRoles() is false)
             {
-                var role = await _dbContext.Roles
-                    .FirstOrDefaultAsync(r => ri == r.Id, cancellationToken);
-                EntityNotFoundException.ThrowIfNull(role, "В системе отсутствует роль с Id={0}", ri);
+                throw new ForbiddenException(
+                    "Текущий пользователь не имеет прав на изменение ролей пользователей");
             }
-            
+
+            var rolesIds = _dbContext.Roles
+                .Select(r => r.Id)
+                .Except(request.RolesIds);
+
+            EntityNotFoundException.ThrowIfNull(rolesIds, "В системе отсутствуют роли с Id={0}", rolesIds);
+
+            if (rolesIds != null && rolesIds.Except(request.RolesIds) is not null)
+            {
+                throw new ForbiddenException(
+                    "При считывании списка новых ролей пользователя произошла ошибка");
+            }
+
             var user = await _dbContext.Users
                 .FirstOrDefaultAsync(u => u.Id == request.UserId, cancellationToken);
 
@@ -45,14 +55,13 @@ public record UpdateUserRolesCommand : IRequest
             var roles = await _dbContext.Roles
                 .Where(r => request.RolesIds.Contains(r.Id))
                 .ToListAsync(cancellationToken);
-            
-            if ((roles.Exists(r => r.Name == "MainAdmin") is false && user.Id == _currentUser.Id)
-                || _currentUser.CanEditUsersRoles() is false)
+
+            if (roles.Exists(r => r.Name == RoleConstants.MainAdmin) is false && user.Id == _currentUser.Id)
             {
                 throw new ForbiddenException(
                     "Текущий пользователь не имеет прав на изменение ролей данного пользователя");
             }
-            
+
             user.UpdateUserRoles(roles);
 
             await _dbContext.SaveChangesAsync(cancellationToken);
